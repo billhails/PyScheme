1fdfe1ee32a3ba2d1e4494decaeea0ad320f2625
Bill Hails <me@billhails.net>
2018-05-19 18:07:59 +0100

    clean-up

--- pyscheme/inference.py
+++ pyscheme/inference.py
@@ -1,3 +1,24 @@
+# PyScheme lambda language written in Python
+#
+# Copyright (C) 2018  Bill Hails
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+# Credit: Rob Smallshire wrote the original python implementation at
+# https://github.com/rob-smallshire/hindley-milner-python
+
+
 from typing import Dict
 from . import expr
 from .exceptions import SymbolNotFoundError, SymbolAlreadyDefinedError, PySchemeInferenceError, PySchemeTypeError
